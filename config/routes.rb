8dacacf3406c4b16ea533ef9dd570b8539d91680
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-13 21:18:03 -0700

    customized registration, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolis, except: [:show]
   get 'portfolio', to: 'portfolis#index'
   get 'portfolio/:id', to: 'portfolis#show', as: 'portfoli_show'
