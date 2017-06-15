b94f94d42fd771aaf452db51e25a5e14220fe6e9
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-15 04:30:12 -0700

    Updated auth to include draft and published status toggle controls

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog" # got to layouts directory and find the file name blog.html
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs # add new branch
   # GET /blogs.json
