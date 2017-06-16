e007799d6a03671134bbba5cbe349f07d4a7dc71
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-16 16:59:14 -0700

    Implemented redis library and created comment generator

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,14 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body
 
   belongs_to :topic, optional: true
+
+  has_many :comment, dependent: :destroy
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
 end
