5d3778906012846117918017c968784add442e34
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-15 23:43:44 -0700

    Updated file upload forms and refactored placeholder functionality into view

--- app/helpers/portfolis_helper.rb
+++ app/helpers/portfolis_helper.rb
@@ -1,2 +1,15 @@
 module PortfolisHelper
+  def image_generator(height:, width: )
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def portfolio_img img, type
+    if img.model.thumb_image? || img.model.main_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200') # thumb_image
+    elsif type == 'main'
+      image_generator(height: '600', width: '400') # main_image
+    end
+  end
 end
