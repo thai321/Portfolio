342c8056c0ec6e2aba9e1a1a6e878b7fc07ec5ab
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-16 23:50:01 -0700

    implemented the blog widget for topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ApplicationRecord
   validates_presence_of :title
   has_many :blogs
+
+  def self.with_blogs # bring me the topic that have blog
+    includes(:blogs).where.not(blogs: {id: nil})
+  end
 end
